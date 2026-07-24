bb7b28fca1d056ec2dc1fa5ff6a68296c8dd1886
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-07-24 22:18:07 +0530

    feat(_socket) : add quic_event_received in RivenConnection - added upgrade to http3 connection if event is ProtocolNegotiated and alpn_protocol is H3 - added checks to check http state before trying to check http3 events if any

--- riven/connections/_socket.py
+++ riven/connections/_socket.py
@@ -15,13 +15,29 @@ from aioquic.h3.connection import (
     H3Connection
 )
 from ..server import Riven
-from ._stream_utils import StreamContext
-
+from ._stream_utils import HTTPStreamContext
+import enum
+import asyncio
 
 class RivenConnection(QuicConnectionProtocol):
     def __init__(self ,manager:Riven ,*args ,**kwargs):
         super().__init__(*args, **kwargs)
         self._manager = manager
-        self._active_streams:dict[int,StreamContext] = dict()
+        self._active_streams:dict[int,HTTPStreamContext] = dict()
         self._http = None
+        self._manager.add_connection(self)
+
+
+    def quic_event_received(self, event):
+        if isinstance(event, ProtocolNegotiated):
+            if event.alpn_protocol == H3_ALPN: # confirm HTTP3
+                self._http = H3Connection(self._quic) # Upgrade to HTTP3
+
+        elif isinstance(event,ConnectionTerminated):
+            asyncio.create_task(self._handle_disconnect(event))
+
+        if self._http:
+            for http_event in self._http.handle_event(event):
+                asyncio.create_task(self.handle_h3_event(http_event))
+
         
