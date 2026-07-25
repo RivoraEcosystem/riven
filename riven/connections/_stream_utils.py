a19574df9732214de4731cbd7ada46f436a22349
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-07-25 21:06:20 +0530

    feat(_stream_utils) : send , receive functions in HTTPStreamContext - changed name for put and get function for queue - used seprate queses for response and request - finished queue when finish method called - declared send function need to be completed

--- riven/connections/_stream_utils.py
+++ riven/connections/_stream_utils.py
@@ -1,4 +1,4 @@
-from rcp import HTTPScope , ScopeType , HTTPVersions , RCPApplication
+from rcp import HTTPScope , ScopeType , HTTPVersions , RCPApplication , RCPSendEvent , RCPReceiveEvent
 from rcp.methods import RequestMethod
 from rcp.scheme import HTTPScheme
 import asyncio
@@ -9,36 +9,66 @@ from _connection_utils import ConnectionInfo
 
 class HTTPStreamContext:
 
-    EOF = object() # EOF Object to mark data's end
+    _REQUEST_EOF = object() # EOF object for marking request's end 
+    _RESPONSE_EOF = object() # EOF object for marking response's end
 
-    def __init__(self,connection_info:ConnectionInfo,stream_id:int,method:RequestMethod,scheme:HTTPScheme,http_version:HTTPVersions):
+    def __init__(self,connection:ConnectionInfo,stream_id:int,method:RequestMethod,scheme:HTTPScheme,http_version:HTTPVersions):
         self.stream_id = stream_id
-        self.connection_info = connection_info
+        self.connection = connection
         self.method = method
         self.scheme = scheme
         self.http_version = http_version
 
-        self._queue = asyncio.Queue(maxsize=10)
+        self._request_queue:asyncio.Queue[RCPReceiveEvent] = asyncio.Queue(maxsize=10)
+        self._response_queue:asyncio.Queue[RCPSendEvent] = asyncio.Queue(maxsize=10)
 
         self.response_started = False
         self.request_forwarded = False
         self.closed = False
         self.task: asyncio.Task | None = None # stores task of Rcp application
 
-    async def add_chunk(self, data:bytes):
-        "Add data to queue buffer"
-        await self._queue.put(data)
+    async def _push_request(self, data:RCPReceiveEvent) -> None:
+        "Add data to request queue buffer"
+        await self._request_queue.put(data)
 
-    async def _finish(self):
+    async def _finish_request(self) -> None:
         "Add none at last to mark request data ending"
-        await self._queue.put(self.EOF)
+        await self._request_queue.put(self._REQUEST_EOF)
+        await self._response_queue.shutdown(immediate=False)
 
-    async def _read(self):
-        "Read from queue buffer to free up queue"
+    async def _pop_request(self) -> RCPReceiveEvent | None:
+        "Read from request queue buffer to free up queue"
         
-        item = await self._queue.get()
+        item = await self._request_queue.get()
     
-        if item is self.EOF: # check if item is EOF then return None
+        if item is self._REQUEST_EOF: # check if item is EOF then return None
             return None
         
         return item
+
+    async def _push_response(self, data:RCPSendEvent) -> None:
+        "Add data to response queue buffer"
+        await self._response_queue.put(data)
+    
+    async def _finish_response(self) -> None:
+        "Add none at last to mark response data ending"
+        await self._response_queue.put(self._RESPONSE_EOF)
+        await self._response_queue.shutdown(immediate=False)
+
+    async def _pop_response(self) -> RCPSendEvent | None:
+        "Read from response queue buffer to free up queue"
+        
+        item = await self._response_queue.get()
+    
+        if item is self._RESPONSE_EOF: # check if item is EOF then return None
+            return None
+        
+        return item
+
+    async def send(self,event:RCPSendEvent):
+        "Send function for RCPApplication which parses events and handle that event forward data accordingly"
+        pass
+
+    async def receive(self) -> RCPReceiveEvent | None:
+        "Receive function for RCPApplication which forward a event from request queue"
+        return await self._pop_request()
