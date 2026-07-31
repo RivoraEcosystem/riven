b4a421189407e233fe7be80d1d5c2eff2bec063f
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-07-31 06:53:18 +0530

    feat(server): add Riven connection manager
    - add rcp application caller function

--- riven/server.py
+++ riven/server.py
@@ -1,4 +1,51 @@
-
+from rcp import (
+    RCPApplication,
+    RCPReceiveCallable,
+    RCPSendCallable,
+    Scope
+)
+from connections import (
+    RivenConnection,
+    ConnectionInfo,
+    HTTPStreamContext,
+    EventHandler
+)
+from .exceptions.exceptions import (
+    RivenException,
+    InvalidEvent,
+    DuplicatePseudoHeader,
+    InvalidPseudoHeader,
+    MethodNotAllowed,
+    InvalidScheme,
+    InvalidAuthority,
+    InvalidPath,
+    InvalidStreamContext
+)
+import asyncio
+from ._config import RivenConfig
+from typing import Any
 
 class Riven: # server connection manager 
-    pass
+    def __init__(
+        self,
+        app:RCPApplication,
+        config:RivenConfig,
+    ):
+        self._application = app
+        self.config = config
+        self._active_connections:dict[bytes,RivenConnection] = dict()
+        self.root_path = ""
+        self.state:dict[str,Any] = None
+        self.extensions:dict[str, dict[object, object]] | None = None
+
+    def add_connection(self,connection:RivenConnection) -> None:
+        self._active_connections[connection._quic.host_cid] = connection
+
+    async def _start_rcp_application(self,scope:Scope,context:HTTPStreamContext) -> asyncio.Task[None]:
+        return asyncio.create_task(
+        self._application(
+            scope,
+            context.receive,
+            context.send,
+        )
+    )
